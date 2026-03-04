d024cc038ef6dbc33a1ca44342016d3bddebf33a
rubenabimalec <al19760767@ite.edu.mx>
2026-03-04 08:09:04 -0800

    fix base for github pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Project_rutas_vue/',
   plugins: [
     vue(),
     vueJsx(),
